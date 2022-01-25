109bd677106c8423e1da8a1c99159ba487fd2016
lzaviriukha <leozaviriukha@ukr.net>
2022-01-25 10:47:18 +0200

    Added button-feture componnt with Icon component

--- my-app/src/components/Button/Button.jsx
+++ my-app/src/components/Button/Button.jsx
@@ -1,7 +1,50 @@
+import s from './Button.module.css';
 
-function Button() {
+
+
+
+function Button(props) {
+
+    const buttonTypeClass = props.type === "primary" && props.disable ? s.btn_primary_disable :
+                            props.type === "primary" && !props.disable ? s.btn_primary : 
+                            props.type === "secondary" && props.disable ? s.btn_secondary_disable :
+                            props.type === "secondary" && !props.disable ? s.btn_secondary: '';
+
+    // let buttonTypeClass;
+    
+    // switch (props.type) {
+    //     case ("primary" && props.disable):
+    //         buttonTypeClass = s.btn_primary_disable;
+    //         break;
+    //     case ("primary" && !props.disable):
+    //         buttonTypeClass = s.btn_primary;
+    //         break;
+    //     case ("secondary" && props.disable):
+    //         buttonTypeClass = s.btn_secondary_disable;
+    //         break;
+    //     case ("secondary" && !props.disable):
+    //         buttonTypeClass = s.btn_secondary;
+    //         break;
+    //     default:
+    //         buttonTypeClass = "";
+    // }
+    
+    
+    const onClicAction = e => {
+        if (props.disable) {
+            e.preventDefault();
+        } else {
+            return props.onClick(e);
+        }
+    }           
+   
+
     return (
-        <div>fgfggf</div>
+            <div className={`${props.className} ${s.btn} ${buttonTypeClass}`}
+                 id={props.id}
+                 onClick={onClicAction}>
+                {props.startIcon}{props.title}{props.endIcon}
+            </div>
     );
 }
 
--- my-app/src/pages/ButtonDemo/ButtonDemo.jsx
+++ my-app/src/pages/ButtonDemo/ButtonDemo.jsx
@@ -1,9 +1,68 @@
 import Button from "../../components/Button/Button";
+import s from '../../components/Button/Button.module.css'
+import Icon from "../../components/Icon/Index";
 
 function ButtonDemo () {
     return (
         <>
-            <Button/>
+            <Button
+                id="my-button" 
+                onClick={() => console.log('log log')}
+                className="someCssCLass" 
+                disable={false}
+                type="primary"
+                title="SAVE"
+                /> 
+
+            <Button
+                id="my-button" 
+                onClick={() => console.log('log log')}
+                className="someCssCLass" 
+                disable={false}
+                type="primary"
+                title="SAVE"
+                endIcon={<Icon name="ForwardArrow" height="14px" width="14px" className={s.btn__icon__end}/>}
+                /> 
+
+            <Button
+                id="my-button" 
+                onClick={() => console.log('log log')}
+                className="someCssCLass" 
+                disable={true}
+                type="primary"
+                title="SAVE"
+                startIcon={<Icon name="Lock" height="14px" width="14px" className={s.btn__icon__start}/>}
+                endIcon={<Icon name="ForwardArrow" height="14px" width="14px" className={s.btn__icon__end}/>}
+                /> 
+
+            <Button
+                id="my-button" 
+                onClick={() => console.log('log log')}
+                className="someCssCLass" 
+                disable={false}
+                type="secondary"
+                title="SAVE"
+                /> 
+            
+            <Button
+                id="my-button" 
+                onClick={() => console.log('log log')}
+                className="someCssCLass" 
+                disable={false}
+                type="secondary"
+                title="SAVE"
+                endIcon={<Icon name="ForwardArrow" height="14px" width="14px" className={s.btn__icon__end}/>}
+                /> 
+            
+            <Button
+                id="my-button" 
+                onClick={() => console.log('log log')}
+                className="someCssCLass" 
+                disable={true}
+                type="secondary"
+                title="SAVE"
+                endIcon={<Icon name="ForwardArrow" height="14px" width="14px" className={s.btn__icon__end}/>}
+                /> 
         </>
     )
 }
