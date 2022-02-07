c320bd6ecdab02bc7f62f40d71d87d89f8e9f594
lzaviriukha <leozaviriukha@ukr.net>
2022-02-07 14:23:10 +0200

    add input/feature

--- my-app/src/components/UI_KIT/Input/Input.jsx
+++ my-app/src/components/UI_KIT/Input/Input.jsx
@@ -1,5 +1,62 @@
-function Input() {
-  return <div>gfgdf</div>;
+import s from "./Input.module.css";
+
+function Input(props) {
+  const {
+    id,
+    label,
+    value,
+    className,
+    onChange,
+    startIcon,
+    endIcon,
+    disabled = false,
+    error = false,
+  } = props;
+
+  const handleOnClick = (evt) => {
+    if (disabled) {
+      evt.preventDefault();
+      evt.stopPropagation();
+    } else {
+      onChange(evt.target.value);
+    }
+  };
+
+  let beforeIcon = null;
+  if (startIcon !== undefined) {
+    beforeIcon = <div className={s.iconBefore}>{props.startIcon}</div>;
+  }
+
+  let afterIcon = null;
+  if (endIcon !== undefined) {
+    afterIcon = <div className={s.iconAfter}>{props.endIcon}</div>;
+  }
+
+  let errorClass = error ? s.error : "";
+  let disabledClass = disabled ? s.disabled : "";
+  let disableValue = disabled ? "disabled" : "";
+  let wrapperClasses = `${s.form__wrapper} ${errorClass} ${disabledClass} ${className.wrapper}`.trim();
+  let inputClasses = `${s.form__input} ${errorClass}`.trim();
+
+  return (
+    <div className={wrapperClasses}>
+      {beforeIcon}
+      <input
+        id={id}
+        value={value}
+        type="text"
+        className={inputClasses}
+        onChange={handleOnClick}
+        disabled={disableValue}
+        placeholder=" "
+      />
+
+      <label htmlFor="" className={s.form__label}>
+        {label}
+      </label>
+      {afterIcon}
+    </div>
+  );
 }
 
 export default Input;
--- my-app/src/pages/InputDemo/InputDemo.jsx
+++ my-app/src/pages/InputDemo/InputDemo.jsx
@@ -1,10 +1,58 @@
+import { useState } from "react";
 import Input from "components/UI_KIT/Input";
+import Icon from "components/UI_Demo/Icon";
+import s from "./InputDemo.module.css";
 
 function InputDemo() {
+  const [firstInputValue, setFirstInputValue] = useState("");
+  const [secondInputValue, setSecondInputValue] = useState("Second input");
+  const [thirdInputValue, setThirdInputValue] = useState("Third input");
+  const [fourthInputValue, setFourthInputValue] = useState("Fourth input");
+
+  const classes = {
+    wrapper: s.wrapper__class
+  };
+
   return (
-    <>
-      <Input />
-    </>
+    <div className={s.formWrapper}>
+      <Input
+        id="first-input"
+        label={"Label"}
+        value={firstInputValue}
+        className={classes}
+        onChange={setFirstInputValue}
+        startIcon={<Icon name="Lock" height="15px" width="18px" />}
+      />
+
+      <Input
+        id="second-input"
+        label={"Label"}
+        value={secondInputValue}
+        className={classes}
+        onChange={setSecondInputValue}
+        endIcon={<Icon name="Lock" height="15px" width="18px" />}
+      />
+
+      <Input
+        id="third-input"
+        label={"Label"}
+        value={thirdInputValue}
+        className={classes}
+        onChange={setThirdInputValue}
+        disabled={true}
+        startIcon={<Icon name="Lock" height="15px" width="18px" />}
+        endIcon={<Icon name="Lock" height="15px" width="18px" />}
+      />
+
+      <Input
+        id="fourth-input"
+        label={"Label"}
+        value={fourthInputValue}
+        className={classes}
+        onChange={setFourthInputValue}
+        error={true}
+      />
+    </div>
   );
 }
 
