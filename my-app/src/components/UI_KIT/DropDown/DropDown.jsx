b29d2a4536c5cf8a07bdb5664f60f20c44e2157f
lzaviriukha <leozaviriukha@ukr.net>
2022-02-15 18:12:27 +0200

    draft dropdown

--- my-app/src/components/UI_KIT/DropDown/DropDown.jsx
+++ my-app/src/components/UI_KIT/DropDown/DropDown.jsx
@@ -1,46 +1,75 @@
 import s from "./DropDown.module.css";
-
-/*
-<Dropdown
-  id="my-dropdown"
-  value='option-id-1'                 // string
-  options={listOfOptions}             // see below
-  onChange={(nextId, nextOption) => { ... }}
-  classes={myClsses}                 // optional, see below
-  disabled={isDisabled}              // optional, boolean
-  getOptionLabel=((option) => { ... }}     // optional, function that returns string
-  isOptionSelected=((option, currentValue) => { ... }}  // optional, function that returns boolean
-  isOptionDisabled=((option, currentValue) => { ... }}  // optional, function that returns boolean
-  expanderIcon={reactElement}        // optional
-/>
-where listOfOptions is:
-
-const listOfOptions = [
-  { id: 'foo', name: 'Foo' },
-  { id: 'bar', name: 'Bar' },
-  { id: 'baz', name: 'Baz' }
-]
-and classes is:
-
-const classses = {
-  wrapper: 'someCssClass',
-  item: 'someCssClass',
-  activeItem: 'someCssClass',
-  disabledItem: 'someCssClass',
-}
-*/
+import { useState } from "react";
 
 function DropDown(props) {
-  const { expanderIcon: icon } = props;
+  const [btnIsActive, setBtnIsActive] = useState(false);
+
+  const {
+    id,
+    value,
+    options,
+    onChange,
+    classes,
+    disabled,
+    getOptionLabel,
+    isOptionSelected = false,
+    isOptionDisabled = false,
+    expanderIcon,
+  } = props;
+
+  const handleOnClickBtn = (evt) => {
+    if (disabled) {
+      evt.preventDefault();
+      evt.stopPropagation();
+    } else {
+      setBtnIsActive(!btnIsActive);
+    }
+  };
+
+  const item = options.map((option) => {
+    const handleOnClick = (evt) => {
+      if (isOptionDisabled(option, value)) {
+        evt.preventDefault();
+        evt.stopPropagation();
+      } else {
+        onChange(option.id, option);
+      }
+    };
+
+    return (
+      <div
+        id={option.id}
+        className={`${s.dropdown_item} ${
+          isOptionSelected(option, value) ? s.dropdown_item__active : ""
+        } ${
+          isOptionDisabled(option, value) ? s.dropdown_item__disabled : ""
+        }`.trim()}
+        onClick={handleOnClick}
+      >
+        {option.name}
+      </div>
+    );
+  });
+
   return (
-    <div className={s.dropdown}>
-      <div className={s.dropdown_btn}>
-        Choose one <div className="dropdown_icon">{icon}</div>
+    <div className={s.dropdown} id={id}>
+      <div
+        className={`${s.dropdown_btn} ${
+          disabled ? s.dropdown__disabled : ""
+        }`.trim()}
+        onClick={handleOnClickBtn}
+      >
+        Choose someone
+        <div
+          className={`${s.dropdown_icon} ${
+            btnIsActive ? s.dropdown_icon__rotate : ""
+          }`.trim()}
+        >
+          {expanderIcon}
+        </div>
       </div>
-      <div className={s.dropdown_content}>
-        <div className={s.dropdown_item}>React</div>
-        <div className={s.dropdown_item}>React</div>
-      </div>
+
+      {btnIsActive && <div className={s.dropdown_content}>{item}</div>}
     </div>
   );
 }
--- my-app/src/pages/DropDownDemo/DropDownDemo.jsx
+++ my-app/src/pages/DropDownDemo/DropDownDemo.jsx
@@ -1,11 +1,64 @@
-import DropDown from 'components/UI_KIT/DropDown';
-import Icon from 'components/UI_Demo/Icon'
-import s from './DropDownDemo.module.css';
+import { useState } from "react";
+import DropDown from "components/UI_KIT/DropDown";
+import Icon from "components/UI_Demo/Icon";
+import s from "./DropDownDemo.module.css";
+
+/*
+<Dropdown
+  classes={myClsses}                 // optional, see below
+  disabled={isDisabled}              // optional, boolean
+  getOptionLabel=((option) => { ... }}     // optional, function that returns string
+/>
+
+*/
 
 function DropDownDemo() {
+  const [activeItemId, setActiveItemId] = useState(null);
+
+  const onChange = (id, option) => setActiveItemId(id);
+  const isOptionSelected = (option, currentValue) => option.id === currentValue ? true : false;
+
+  const isOptionDisabled = (option, currentValue) => {
+      if (option.disabled && (option.id === currentValue)) {
+        setActiveItemId(null);
+        return true;
+      } else if (option.disabled) {
+        return true;
+      } else {
+        return false;
+      }
+  };
+
+  const getOptionLabel = (option) => option.name;
+  
+
+  const listOfOptions = [
+    { id: 'foo', name: 'Foo', disabled: false },
+    { id: 'bar', name: 'Bar', disabled: false },
+    { id: 'baz', name: 'Baz', disabled: true }
+  ]
+
+  const classses = {
+    wrapper: 'someCssClass',
+    item: 'someCssClass',
+    activeItem: 'someCssClass',
+    disabledItem: 'someCssClass',
+  }
+
   return (
-    <DropDown expanderIcon={<Icon name="DropArrow"/>}/>
-  )
+    <DropDown
+      id="my-dropdown"
+      value={activeItemId}
+      options={listOfOptions}
+      onChange={onChange}
+      classes={classses}
+      disabled={false}
+      getOptionLabel={getOptionLabel}
+      isOptionSelected={isOptionSelected}
+      isOptionDisabled={isOptionDisabled}
+      expanderIcon={<Icon name="DropArrow" />}
+    />
+  );
 }
 
 export default DropDownDemo;
